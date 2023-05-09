4d812dc7b1439fcbcaf37609937d6e8d00ed4dd7
cp.vibrantsofttech <cp.vibrantsofttech@gmail.com>
2023-05-09 13:08:58 +0530

    ddd

--- src/components/Pages/Home/Banner.tsx
+++ src/components/Pages/Home/Banner.tsx
@@ -8,111 +8,26 @@ function Banner() {
                 <div className="main-slides-item">
                     <div className="container">
                         <div className="main-slides-content">
-                            <h1>&nbsp;</h1>
-                            <p>&nbsp;</p>
-                            <h1>&nbsp;</h1>
-                            <p>&nbsp;</p>
-                            <h1>&nbsp;</h1>
-                            <p>&nbsp;</p>
-                            <div className="slides-btn">
-                                &nbsp;&nbsp;
-                                {/* <a href="appointment.html" className="default-btn">Book Appointment</a> */}
-                                {/* <div className="checkup-content">
-                                    <div className="icon">
-                                        <i className="flaticon-chair"></i>
-                                    </div>
-                                    <span><a href="contact-us.html">Get Free Checkup</a></span>
-                                </div> */}
-                            </div>
+                            <h1>We learn to <span>Save</span></h1>
                         </div>
                     </div>
                 </div>
                 <div className="main-slides-item item-bg2">
                     <div className="container">
                         <div className="main-slides-content">
-                            <h1>&nbsp;</h1>
-                            <p>&nbsp;</p>
-                            <h1>We learn to <span>Save</span></h1>
-                            <div className="slides-btn">
-                                {/* <a href="appointment.html" className="default-btn">Book Appointment</a> */}
-                                {/* <div className="checkup-content">
-                                    <div className="icon">
-                                        <i className="flaticon-chair"></i>
-                                    </div>
-                                    <span><a href="contact-us.html">Get Free Checkup</a></span>
-                                </div> */}
-                            </div>
-                            <h1>&nbsp;</h1>
-                            <p>&nbsp;</p>
+                            <h1>Be Alert <br /><span>Avert Fire</span></h1>
                         </div>
                     </div>
                 </div>
                 <div className="main-slides-item item-bg3">
                     <div className="container">
                         <div className="main-slides-content">
-                            <h1>&nbsp;</h1>
-                            <p>&nbsp;</p>
                             <h1>Keep Calm and  <span>Fire Safety On</span></h1>
-                            <div className="slides-btn">
-                                {/* <div className="checkup-content">
-                                    <div className="icon">
-                                        <i className="flaticon-chair"></i>
-                                    </div>
-                                    <span><a href="contact-us.html">Get Free Checkup</a></span>
-                                </div> */}
-                            </div>
-                            <h1>&nbsp;</h1>
-                            <p>&nbsp;</p>
                         </div>
                     </div>
                 </div>
             </div>
 
-            {/* <section className="features-area pb-70">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-4 col-md-6">
-                            <div className="single-features">
-                                <div className="content">
-                                    <div className="icon">
-                                        <img src="assets/images/others/si3.png" className="card-image" alt="logo" />
-                                    </div>
-                                    <h3>
-                                        <a href="services-details.html">Best Fire Institute</a>
-                                    </h3>
-                                    <p>CIFSE Is Best Fire Institute Situated In India Provided Good Result And placement Guarantee</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6">
-                            <div className="single-features bg-f57e57">
-                                <div className="content">
-                                    <div className="icon">
-                                        <img src="assets/images/others/training1.png" className="card-image" alt="logo" />
-                                    </div>
-                                    <h3>
-                                        <a href="services-details.html">Our Courses</a>
-                                    </h3>
-                                    <p>CIFSE Provided Different Types Of Fire,Safety & Environmental Engineering Courses</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6 offset-lg-0 offset-md-3">
-                            <div className="single-features bg-4a6577">
-                                <div className="content">
-                                    <div className="icon">
-                                        <img src="assets/images/others/docs2.png" className="card-image" alt="logo" />
-                                    </div>
-                                    <h3>
-                                        <a href="services-details.html">Admission Process</a>
-                                    </h3>
-                                    <p>The Admission Procedure In CIFSE Is Very Easy And Simple Just Fill The Form & Apply</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section> */}
         </>
     )
 }
