e437902c47d4949d578201388743ae8714f9ee09
Harshvardhan <info@samcomtechnologies.com>
2024-07-22 18:42:57 +0530

    test auto build script

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -18,7 +18,7 @@ function Header() {
                                 </li>
                                 <li>
                                     <i className="fa fa-envelope" aria-hidden="true"></i>
-                                    <span className='ms-1'><a href="mailto:info@cifse.in">info@cifse .in</a></span>
+                                    {/* <span className='ms-1'><a href="mailto:info@cifse.in">info@cifse .in</a></span> */}
                                 </li>
                             </ul>
                         </div>
