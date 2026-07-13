e8133307f741b6a6b2e8040334469375a05ccecb
Harshvardhan <121049895+oneheavysoul@users.noreply.github.com>
2026-07-13 15:44:56 +0530

    Update stud.tsx
    1 result added

--- src/components/Pages/StudentResult/stud.tsx
+++ src/components/Pages/StudentResult/stud.tsx
@@ -2954,6 +2954,26 @@ const studentData = [
       { name: "Practical 3", marks: 61 }
     ]
   },
+{
+    id: 152,
+    name: "GALTHARIYA DILIP GORDHANBHAI",
+    course: "D.F.S.E.",
+    batch: "2025/2026",
+    enrollment_no: "CIFSE00AS025",
+    subjects: [
+      { name: "F.E.S.C", marks: 75 },
+      { name: "S.O.A", marks: 74 },
+      { name: "S.B.C", marks: 76 },
+      { name: "F.F.F.I", marks: 82 },
+      { name: "S.F.H", marks: 69 },
+      { name: "R.E.M", marks: 78 },
+      { name: "F.A.P", marks: 72 },
+      { name: "S.M", marks: 80 },
+      { name: "Practical 1", marks: 70 },
+      { name: "Practical 2", marks: 68 },
+      { name: "Practical 3", marks: 61 }
+    ]
+  },
 ];
 
 
